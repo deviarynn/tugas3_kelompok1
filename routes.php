74394ee0c1009338b7804fc33201ae454d4ab16a
RevanoAugustofa <revanoa4945@gmail.com>
2024-12-19 09:09:08 +0700

    routes attendees

--- routes.php
+++ routes.php
@@ -54,3 +54,32 @@ if ($url == '/event/index') {
 } else {
     http_response_code(404);
 }
+
+
+require_once 'app/controllers/AttendsController.php';
+
+$controller = new AttendsController();
+$url = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+if ($url == '/peserta/index') {
+    $controller->index();
+} elseif ($url == '/peserta/create' && $requestMethod == 'GET') {
+    $controller->create();
+} elseif ($url == '/peserta/store' && $requestMethod == 'POST') {
+    $controller->store();
+} elseif (preg_match('/\/peserta\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $pesertaId = $matches[1];
+    $controller->edit($pesertaId);
+} elseif (preg_match('/\/peserta\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
+    $pesertaId = $matches[1];
+    $controller->update($pesertaId, $_POST);
+} elseif (preg_match('/\/peserta\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
+    $pesertaId = $matches[1];
+    $controller->delete($pesertaId);
+} elseif ($url == '/') {
+    $controller->dashboard();
+} else {
+    http_response_code(404);
+    echo "404 Not Found";
+}
